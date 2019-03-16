4172f8f46961402ed3a1b70643be0ee6aac496d8
Harry Hong <harry851025@gmail.com>
2019-03-16 22:13:59 +0800

    Add files via upload

---
+++ co_hw2_2018/Shifter.v
@@ -0,0 +1,13 @@
+module Shifter( result, leftRight, shamt, sftSrc  );
+    
+  	output wire[31:0] result;
+
+  	input wire leftRight;
+  	input wire[4:0] shamt;
+  	input wire[31:0] sftSrc ;
+	
+  	
+	assign result= (leftRight==0) ? sftSrc >> shamt : sftSrc << shamt;
+
+
+endmodule 
